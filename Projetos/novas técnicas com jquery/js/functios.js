ff2005a19ce391c4d030033f688c88862aa96527
Guilherme-alexander <guilhermealexanderdsn@gmail.com>
2021-06-25 00:23:12 -0300

    add New Project

--- Projetos/novas técnicas com jquery/js/functios.js
+++ Projetos/novas técnicas com jquery/js/functios.js
@@ -1,7 +1,10 @@
 $(function(){
 
+// Função abrir e fechar Janela
 	abrirJanela();
 	validarClickExit();
+
+// Função abrir janela
 	function abrirJanela(){
 		$('.btn').click(function(e){
 			e.stopPropagation(); 
@@ -9,6 +12,7 @@ $(function(){
 		});
 	}
 
+// Função fechar Janela
 	function validarClickExit(){
 		var el = $('body,.closeBtn');
 			el.click(function(){
@@ -17,58 +21,95 @@ $(function(){
 
 		$('form').click(function(e){
 			e.stopPropagation();
-		})
+		});
 	}
 
+// Função resetar campos invalidos focado
+	$('input[type=text]').focus(function(){
+		resetarCampoInvalido($(this));
+	});
+
+
+// Validação de formularios
 	$('form#form1').submit(function(e){
-		e.preventDefalt()
+		//e.preventDefault();
 		var nome = $('input[name=nome]').val();
 		var telefone = $('input[name=telefone]').val();
 		var email = $('input[name=email]').val();
 
 		if(verificarNome(nome) == false){
-			aplicarCampoInvalido($('input[name=nome]'))
+			aplicarCampoInvalido($('input[name=nome]'));
+			return false;
+		}else if(verificarTelefone(telefone) == false){
+			aplicarCampoInvalido($('input[name=telefone]'));
+			return false;
+		}else if(verificarEmail(email) == false){
+			aplicarCampoInvalido($('input[name=email]'));
+			return false;
 		}else{
-			alert("formulário enviado com sucesso!")
+			alert('formulario Enviado Com sucesso!')
 		}
 
 	});
 
-	function verificarNome(nome){
-		//validando Name
-		if(nome == ''){
-			return false;
-		}
-		var amount = nome.split(' ').length;
-		var splitStr = nome.split(' ');
-		if(amount >= 2){
-			for(var i = 0; i < amount; i++){
-				if(splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)){
-				}else{
-					return false;
-				}
-			}
-		}else{
-			return false;
-		}
-	}
-
+// aplicando campo invalido
 	function aplicarCampoInvalido(el){
-		el.css('border','2px solid red');
-		el.css('color','red');
+		el.css('color','#f00');
+		el.css('border','2px solid #f00');
 		//el.data('invalido','true');
-		el.val('campo invalido!')
+		el.val('Campo Invalido!');
 	}
 
-	function reaetarCampoInvalido(el){
-		el.css('border','1px solid #ccc');
-		el.css('color','black');
-		//el.data('invalido','true');
-		el.val('')
+// Função resetar campos invalidos
+function resetarCampoInvalido(el){
+	el.css('color','#000');
+	el.css('border','1px solid #ccc');
+	//el.data('invalido','true');
+	el.val('');
+}
+
+
+// função verificar os campos
+function verificarNome(nome){
+	//contando valores dos caracteres
+	if(nome == ''){
+		return false;
+	}
+	var amount = nome.split(' ').length;
+	var splitStr = nome.split(' ');
+	if(amount >= 2){
+		for(var i = 0; i < amount; i++){
+			if(splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)){
+				//console.log('condição ok')
+
+			}else{
+				return false;
+			}
+		}
+	}else{
+		return false;
+	}
+}	
+
+	function verificarTelefone(telefone){
+		//contando valores dos caracteres
+		if(telefone == ''){
+			return false;
+		}
+		if(telefone.match(/^[0-9]{2}[0-9]{4}[0-9]{4}$/) == null){
+			return false;
+		}
 	}
 
-	$('input[type=text]').focus(function(){
-		reaetarCampoInvalido($(this))
-	})
+	function verificarEmail(email){
+		if(email = ''){
+			return false;
+		}
+		if(email.match(/^([a-z0-9-_.]{1,})+@+([a-z.])$/) == null){
+			return false;
+		}
+	}
+
+	
 
 });
