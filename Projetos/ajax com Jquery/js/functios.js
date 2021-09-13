a6d6b1885a516b6a3cf6f7274097295c71643fa1
Guilherme-alexander <guilhermealexanderdsn@gmail.com>
2021-09-13 00:15:47 -0300

    enviando mais projetos

--- Projetos/ajax com Jquery/js/functios.js
+++ Projetos/ajax com Jquery/js/functios.js
@@ -6,19 +6,34 @@ $('button').click(function(){
 		//method:"POST",
 		type:'GET',
 		url:'https://pokeapi.co/api/v2/pokemon/',
-		//url:'conteudo.html',
-		data:{'name':'',
-				'id':''}
+		data:{
+			'name':'',
+			'results':'',
+			'count':'',
+			'id':''}
 
 	}).done(function(pokemon){
+
+
 		
 		var valor = $('input[type=number]').val()
-		pokemon.id = valor
-		pokemon.id.val = `https://pokeapi.co/api/v2/pokemon/${pokemon.id}`
-		
-		$('.container').append('<li>'+'<h2>'+pokemon.id +'</h2>'+`<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png">`+'</li>')
 
-		//
+		pokemon.id = valor
+		//pokemon.id.val = `https://pokeapi.co/api/v2/pokemon/${pokemon.id}`
+		
+		
+
+		if(pokemon.id == 0){
+			alert('value undefined');
+
+		}else{
+			$('.container').append('<li>'+'<h2>'+ pokemon.id +'</h2>'+`<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png">`+'<h2>'+ pokemon.name +'</h2>'+'</li>');
+
+			console.log(pokemon.results)
+
+		}
+	
+
 	//* $('.container').append('<li>'+'<h2>'+pokemon.id +'</h2>'+`<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemon.id}.png">`+'</li>')*/
 		
 
